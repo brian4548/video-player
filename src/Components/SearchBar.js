e8f467550b75a81d549b41e9c17278fb289a0f53
brian4548 <brianr4548@gmail.com>
2021-07-13 23:57:35 -0500

    added click even with controlled input

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,13 +1,22 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = {
+    term: "",
+  };
   render() {
     return (
       <div className="search-bar ui segment">
         <form className="ui form">
           <div className="field">
-              <label>Search Videos</label>
-            <input type="text" />
+            <label>Search Videos</label>
+            <input
+              value={this.state.value}
+              type="text"
+              onChange={(event) => {
+                this.setState({ term: event.target.value });
+              }}
+            />
           </div>
         </form>
       </div>

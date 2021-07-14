d8873c84ebea8ffc6b73096b2bde64c48b041f6a
brian4548 <brianr4548@gmail.com>
2021-07-13 23:53:00 -0500

    quick boilerplate scaffold

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+              <label>Search Videos</label>
+            <input type="text" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;

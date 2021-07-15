177a23ace3b5c2911f627d3d6bb6bd6923f6a6fb
brian4548 <brianr4548@gmail.com>
2021-07-14 20:31:28 -0500

    axios get request to youtube data v3 api

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,25 @@
+import axios from 'axios'
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from '../apis/youtube';
+
 
 class App extends React.Component {
+
+  handleTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    })
+    console.log(response)
+  }
+
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.handleTermSubmit}/>
       </div>
     );
   }
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -4,18 +4,26 @@ class SearchBar extends React.Component {
   state = {
     term: "",
   };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onFormSubmit(this.state.term)
+  };
+  handleInputChange = (event) => {
+    this.setState({ term: event.target.value });
+    //make sure we call callback from parent component 
+  };
+
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.handleFormSubmit}>
           <div className="field">
             <label>Search Videos</label>
             <input
               value={this.state.value}
               type="text"
-              onChange={(event) => {
-                this.setState({ term: event.target.value });
-              }}
+              onChange={this.handleInputChange}
             />
           </div>
         </form>
